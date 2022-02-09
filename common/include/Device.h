93a59fad4bc1fd3e4b2c437a4f0b57511bc6c8f2
Carmelo Fernandez Aguera <carferna@microsoft.com>
2022-02-08 17:18:54 -0800

    DMA channel definitions

--- common/include/Device.h
+++ common/include/Device.h
@@ -72,6 +72,24 @@ namespace IO
     using BG2P      = AffineTxRegister<0x4000020>;
     using BG3P      = AffineTxRegister<0x4000030>;
 
+	// DMA
+	using DMA0SAD = IORegister<uint32_t, 0x40000B0>; // Source address
+	using DMA0DAD = IORegister<uint32_t, 0x40000B4>; // Destination address
+	using DMA0CNT_L = IORegister<uint16_t, 0x40000B8>;
+	using DMA0CNT_H = IORegister<uint16_t, 0x40000BA>;
+	using DMA1SAD = IORegister<uint32_t, 0x40000BC>; // Source address
+	using DMA1DAD = IORegister<uint32_t, 0x40000C0>; // Destination address
+	using DMA1CNT_L = IORegister<uint16_t, 0x40000C4>;
+	using DMA1CNT_H = IORegister<uint16_t, 0x40000C6>;
+	using DMA2SAD = IORegister<uint32_t, 0x40000C8>; // Source address
+	using DMA2DAD = IORegister<uint32_t, 0x40000CC>; // Destination address
+	using DMA2CNT_L = IORegister<uint16_t, 0x40000D0>;
+	using DMA2CNT_H = IORegister<uint16_t, 0x40000D2>;
+	using DMA3SAD = IORegister<uint32_t, 0x40000D4>; // Source address
+	using DMA3DAD = IORegister<uint32_t, 0x40000D8>; // Destination address
+	using DMA3CNT_L = IORegister<uint16_t, 0x40000DC>;
+	using DMA3CNT_H = IORegister<uint16_t, 0x40000DE>;
+
     // Keypad
     using KEYINPUT  = IORegister<uint16_t, 0x4000130>;
     using KEYCNT    = IORegister<uint16_t, 0x4000132>;
@@ -83,6 +101,33 @@ namespace IO
     using IME       = IORegister<uint16_t, 0x4000208>;
 
 }   // namespace IO
+
+namespace DMA
+{
+	struct Channel
+	{
+		volatile uint32_t srcAddress;
+		volatile uint32_t dstAddress;
+		volatile uint16_t wordCount;
+		volatile uint16_t control;
+	};
+
+	inline Channel& Channel0() {
+		return *reinterpret_cast<Channel*>(IO::DMA0SAD::address);
+	}
+
+	inline Channel& Channel1() {
+		return *reinterpret_cast<Channel*>(IO::DMA1SAD::address);
+	}
+
+	inline Channel& Channel2() {
+		return *reinterpret_cast<Channel*>(IO::DMA2SAD::address);
+	}
+
+	inline Channel& Channel3() {
+		return *reinterpret_cast<Channel*>(IO::DMA3SAD::address);
+	}
+}
 
 static constexpr uint32_t PaletteMemAddress = 0x05000000;
 static constexpr uint32_t PaletteMemSize = 0x400;
