97bb855b7b14ea49d64235cc845b21c5be1a7b31
technik <cjtech90@gmail.com>
2022-03-16 20:58:25 -0700

    Fix link errors

---
+++ common/include/font.h
@@ -0,0 +1,14 @@
+
+//{{BLOCK(font)
+
+#ifndef __FONT__
+#define __FONT__
+
+extern const TFont fontFont;
+
+#define fontGlyphsLen 131072
+extern const unsigned int fontGlyphs[32768];
+
+#endif // __FONT__
+
+//}}BLOCK(font)
