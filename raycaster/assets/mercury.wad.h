1ada4382dcea9720049ab3d1230c3a9b3eedad67
Carmelo Fernandez Aguera <cjtech90@gmail.com>
2022-10-02 20:49:28 -0700

    Updated mercury map

--- raycaster/assets/mercury.wad.h
+++ raycaster/assets/mercury.wad.h
@@ -3,25 +3,25 @@
 #include <base.h>
 #include <WAD.h>
 
-constexpr uint32_t mercury_WADVerticesSize = 324;
+constexpr uint32_t mercury_WADVerticesSize = 694;
 extern const uint32_t mercury_WADVertices[];
 
-constexpr uint32_t mercury_WADLineDefsSize = 648;
+constexpr uint32_t mercury_WADLineDefsSize = 1176;
 extern const uint32_t mercury_WADLineDefs[];
 
-constexpr uint32_t mercury_WADSideDefsSize = 2625;
+constexpr uint32_t mercury_WADSideDefsSize = 3825;
 extern const uint32_t mercury_WADSideDefs[];
 
-constexpr uint32_t mercury_WADSegmentsSize = 1113;
+constexpr uint32_t mercury_WADSegmentsSize = 1698;
 extern const uint32_t mercury_WADSegments[];
 
-constexpr uint32_t mercury_WADSubSectorsSize = 108;
+constexpr uint32_t mercury_WADSubSectorsSize = 176;
 extern const uint32_t mercury_WADSubSectors[];
 
-constexpr uint32_t mercury_WADSectorsSize = 286;
+constexpr uint32_t mercury_WADSectorsSize = 397;
 extern const uint32_t mercury_WADSectors[];
 
-constexpr uint32_t mercury_WADNodesSize = 963;
+constexpr uint32_t mercury_WADNodesSize = 1575;
 extern const uint32_t mercury_WADNodes[];
 
 void loadMap_mercury_WAD(WAD::LevelData& dstLevel);
--- raycaster/source/SectorRasterizer.cpp
+++ raycaster/source/SectorRasterizer.cpp
@@ -38,7 +38,7 @@ void SectorRasterizer::EndFrame()
     displayMode.Flip();
 }
 
-#define LEVEL 2
+#define LEVEL 1
 
 bool loadWAD(WAD::LevelData& dstLevel)
 {
