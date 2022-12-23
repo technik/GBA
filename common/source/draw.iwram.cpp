0ec04d2c623af32c84a9a5d577c022e711fe1cbd
technik <cjtech90@gmail.com>
2022-12-23 17:07:53 +0100

    Fixed triangle bounding box. Improved performance by a bit

--- common/source/draw.iwram.cpp
+++ common/source/draw.iwram.cpp
@@ -8,7 +8,6 @@ void rasterTriangle(uint16_t* dst, math::Vec2i scissor, uint16_t color, const ma
 	auto e0 = v[1] - v[0];
 	auto e1 = v[2] - v[1];
 	auto e2 = v[0] - v[2];
-	math::Vec2p8 p;
 
 	if (cross(e0, e1) >= 0)
 		return;
@@ -17,17 +16,17 @@ void rasterTriangle(uint16_t* dst, math::Vec2i scissor, uint16_t color, const ma
 	auto x0 = v[0].x;
 	auto x1 = v[1].x;
 	auto x2 = v[2].x;
-	auto xStart = math::max(0_p8, math::min(x0, math::min(x1, x2)) - 0.5_p8).floor();
-	auto xEnd = math::min(intp8(scissor.x), math::max(x0, math::max(x1, x2)) - 0.5_p8).floor() + 1;
+	auto xStart = math::max(0_p8, math::min(x0, math::min(x1, x2))-0.5_p8).floor();
+	auto xEnd = math::min(intp8(scissor.x), math::max(x0, math::max(x1, x2)) - 0.5_p8).floor();
 
 	auto y0 = v[0].y;
 	auto y1 = v[1].y;
 	auto y2 = v[2].y;
-
 	auto yStart = math::max(0_p8, math::min(y0, math::min(y1, y2)) - 0.5_p8).floor();
-	auto yEnd = math::min(intp8(scissor.y), math::max(y0, math::max(y1, y2)) - 0.5_p8).floor() + 1;
+	auto yEnd = math::min(intp8(scissor.y), math::max(y0, math::max(y1, y2)) - 0.5_p8).floor();
 
 	// Parse bounding rectangle looking for intersections
+	math::Vec2p8 p;
 	for (auto y = yStart; y < yEnd; y += 1)
 	{
 		p.y = intp8(y);
@@ -36,9 +35,15 @@ void rasterTriangle(uint16_t* dst, math::Vec2i scissor, uint16_t color, const ma
 			p.x = intp8(x);
 
 			if (cross(p - v[0], e0) > 0)
+			{
 				if (cross(p - v[1], e1) > 0)
+				{
 					if (cross(p - v[2], e2) > 0)
+					{
 						dst[x + y * scissor.x] = color;
+					}
+				}
+			}
 		}
 	}
 }
