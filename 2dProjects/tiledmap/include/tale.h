110dc74b6ac46ba57a7113939b20e32a971bd740
technik <cjtech90@gmail.com>
2024-02-12 18:20:52 -0800

    test tilemap assets

---
+++ 2dProjects/tiledmap/include/tale.h
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+extern const uint32_t talePalette[];
+
+constexpr uint32_t taleTileSize = 5056;
+extern const uint32_t taleTileData[];
+
+constexpr uint32_t taleTileCount = 316;
---
+++ 2dProjects/tiledmap/tiles/riverTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="riverTiles" tilewidth="8" tileheight="8" tilecount="320" columns="32">
+ <image source="tale-Tileset.png" width="256" height="80"/>
+</tileset>
